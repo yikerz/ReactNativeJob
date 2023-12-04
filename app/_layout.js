44ae2b68cdba1fdb9bfa2c00e50deb8ea01d9fc6
Travis LunMong <travis.lunmong@fdmgroup.com>
2023-12-04 16:00:40 +1100

    Initial project setup

---
+++ app/_layout.js
@@ -0,0 +1,7 @@
+/* 2. Create layout */
+import { Stack } from "expo-router/stack";
+
+/* 3. Define default function */
+export default function Layout() {
+  return <Stack />;
+}
---
+++ app/index.js
@@ -0,0 +1,11 @@
+/* 2. Create Home page */
+import { Text, View } from "react-native";
+
+/* 4. Define Home function */
+export default function Home() {
+  return (
+    <View>
+      <Text>Home</Text>
+    </View>
+  );
+}
